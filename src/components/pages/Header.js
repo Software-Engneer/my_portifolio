ee67d8e6da6a8dd9aae3129911b171da82f133e3
chikondi <chikondimatumula1994@gmail.com>
2025-06-20 14:09:53 +0200

    add modified files for better responsiveness

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
     };
 
     const handleResize = () => {
-      if (window.innerWidth > 900) {
+      if (window.innerWidth > 200) {
         setIsMenuOpen(false);
       }
     };
