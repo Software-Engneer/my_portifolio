449049651418c65375635bd4d0d16c0379ae4f10
chikondi <chikondimatumula1994@gmail.com>
2025-06-20 14:20:57 +0200

    add modified files for better responsiveness

--- src/components/pages/Layout.js
+++ src/components/pages/Layout.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./Header";
 import styles from "./Layout.module.css";
 
 function Layout({ children }) {
   return (
     <div className={styles.layout}>
-      <Header />
       <main className={styles.main}>
         {children}
       </main>
