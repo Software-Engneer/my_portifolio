1bcdd7557c2a1c2df9c54b19722307547d76847b
chikondi <chikondimatumula1994@gmail.com>
2025-07-09 05:38:37 +0200

    adding modified file

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { ClipLoader } from 'react-spinners';
 
-const Spinner = ({ size = 64, color = '#888', loading = true, style = {} }) => (
+const Spinner = ({ size = 90, color = '#888', loading = true, style = {} }) => (
   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 100, ...style }}>
-    <ClipLoader size={size} color={color} loading={loading} />
+    <ClipLoader 
+      size={size} 
+      color={color} 
+      loading={loading} 
+      cssOverride={{ borderWidth: '8px' }}
+    />
   </div>
 );
 
